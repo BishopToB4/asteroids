7c2ffa874c545c4f5d3711f1dc77c748979ffa78
BishopToB4 <bishoptob4@protonmail.com>
2022-12-21 22:14:39 +0000

    Added max speed animation

--- project/src/Ship.gd
+++ project/src/Ship.gd
@@ -4,6 +4,7 @@ export var rotate_speed = 5
 var velocity = Vector2.ZERO
 
 func _ready():
+	$AnimatedSprite.play()
 	pass 
 	
 func _physics_process(delta):
@@ -12,12 +13,13 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("accelerate"):
 		velocity = lerp(velocity, transform.x * 300, 1 * delta)
-		$AnimatedSprite.animation = "accelerate"
-		$AnimatedSprite.play()
+		if $AnimatedSprite.animation != "max_speed":
+			$AnimatedSprite.animation = "accelerate"
 	elif velocity != Vector2.ZERO:
 		$AnimatedSprite.animation = "decelerate"
 	else: 
 		$AnimatedSprite.animation = "default"	
+		
 	
 	position += velocity * delta
 	velocity = lerp(velocity, Vector2.ZERO, 1 * delta)
@@ -34,3 +36,8 @@ func _physics_process(delta):
 	#move_and_slide(velocity)	
 	#position.x = clamp(position.x, 0, screen_size.x)
 	#position.y = clamp(position.y, 0, screen_size.y)
+
+
+func _on_AnimatedSprite_animation_finished():
+	if $AnimatedSprite.animation == "accelerate":
+		$AnimatedSprite.animation = "max_speed"
