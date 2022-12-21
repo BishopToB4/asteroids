0e9ec1ca3b1ee6ae8c7d07192079cb6399b62d16
BishopToB4 <bishoptob4@protonmail.com>
2022-12-21 21:27:05 +0000

    Ship can now rotate and move with animation

--- project/src/Ship.gd
+++ project/src/Ship.gd
@@ -1,29 +1,35 @@
 extends KinematicBody2D
 
-var speed = 40
-var rotation_speed = 1.5
+export var rotate_speed = 5
 var velocity = Vector2.ZERO
-var rotation_direction = 0
 
 func _ready():
 	pass 
-
+	
 func _physics_process(delta):
+	var rot = Input.get_axis("rotate_anticlockwise","rotate_clockwise") * rotate_speed
+	rotate(rot * delta)
 	
 	if Input.is_action_pressed("accelerate"):
-		velocity.x += 1
-	if Input.is_action_pressed("rotate_clockwise"):
-		rotation_degrees += 5	
-	if Input.is_action_pressed("rotate_anticlockwise"):
-		rotation_degrees -= 5	
-			
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
+		velocity = lerp(velocity, transform.x * 300, 1 * delta)
 		$AnimatedSprite.animation = "accelerate"
-		$AnimatedSprite.play()	
-	else:
-		$AnimatedSprite.stop()
-		$AnimatedSprite.animation = "default"
+		$AnimatedSprite.play()
+	elif velocity != Vector2.ZERO:
+		$AnimatedSprite.animation = "decelerate"
+	else: 
+		$AnimatedSprite.animation = "default"	
+	
+	position += velocity * delta
+	velocity = lerp(velocity, Vector2.ZERO, 1 * delta)
+
+			
+	#if velocity.length() > 0:
+#		velocity = velocity.normalized() * speed
+#		$AnimatedSprite.animation = "accelerate"
+#		$AnimatedSprite.play()	
+#	else:
+#		$AnimatedSprite.stop()
+#		$AnimatedSprite.animation = "default"
 		
 	#move_and_slide(velocity)	
 	#position.x = clamp(position.x, 0, screen_size.x)
